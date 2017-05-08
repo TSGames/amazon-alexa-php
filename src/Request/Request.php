2a18df1e5bbf1ee7bf9226dc3b06f5aef8c3664a
TSGames <anfragen@tsgames.de>
2017-05-08 12:28:48 +0200

    Added signature verification

--- src/Request/Request.php
+++ src/Request/Request.php
@@ -7,8 +7,8 @@ use InvalidArgumentException;
 use DateTime;
 
 abstract class Request {
-	const TIMESTAMP_VALID_TOLERANCE_SECONDS = 30;
-
+	const TIMESTAMP_VALID_TOLERANCE_SECONDS = 15;
+	const SAN_NAME="DNS:echo-api.amazon.com";
 	public $requestId;
 	public $timestamp;
 	public $user;
@@ -17,13 +17,14 @@ abstract class Request {
 		$this->requestId = $data['request']['requestId'];
 		$this->timestamp = new DateTime($data['request']['timestamp']);
 		$this->user = new User($data['session']['user']);
+		$this->validate();
 	}
 
 	public static function fromData($data) {
 		$requestType = $data['request']['type'];
 
 		if (!class_exists('\\Alexa\\Request\\' . $requestType)) {
-			throw new RuntimeException('Unknown request type: ' . $requestType);
+			self::handleException(new RuntimeException('Unknown request type: ' . $requestType));
 		}
 
 		$className = '\\Alexa\\Request\\' . $requestType;
@@ -31,17 +32,54 @@ abstract class Request {
 		$request = new $className($data);
 		return $request;
 	}
-
+	public static function handleException($exception){
+		http_response_code(400);
+		echo $exception;
+		die();
+	}
 	public function validate() {
 		$this->validateTimestamp();
+		$this->validateSignature();
 	}
-
+	private function validateCertificateUrl($url){
+		$url=parse_url($url);
+		$valid=$url["scheme"]=="https";
+		$valid&=strtolower($url["host"])==strtolower("s3.amazonaws.com");
+		$valid&=$url["port"]==443 || $url["port"]==null;
+		$valid&=strpos($url["path"],"/echo.api/")===0;
+		if(!$valid)
+			self::handleException(new InvalidArgumentException('Wrong Signature certificate url'));
+		
+	}
+	private function validateSignature() {
+		$url=getallheaders()["Signaturecertchainurl"];
+		$this->validateCertificateUrl($url);
+		
+		$cert=file_get_contents($url);
+		$ssl=openssl_x509_parse($cert);
+		$key=openssl_pkey_get_public($cert);
+		$validFrom=$ssl['validFrom_time_t'];
+		$validTo=$ssl['validTo_time_t'];
+		if($validFrom>time() || $validTo<time()){
+			self::handleException(new InvalidArgumentException('Wrong Signature timestamps'));
+		}
+		if($ssl["extensions"]["subjectAltName"]!=self::SAN_NAME){
+			self::handleException( new InvalidArgumentException('Wrong Subject Alternative Names'));
+		}
+		$signature=base64_decode(getallheaders()["Signature"]);
+		$result='';
+		$hash=sha1(file_get_contents('php://input'),true);
+		openssl_public_decrypt($signature,$result,$key);
+		$result=substr($result,15);
+		if($result!=$hash){
+			self::handleException(new InvalidArgumentException('Wrong Signature hash'));
+		}
+	}
 	private function validateTimestamp() {
 		$now = new DateTime;
-		$differenceInSeconds = $now->getTimestamp() - $this->timestamp->getTimestamp();
-
+		$differenceInSeconds = abs($now->getTimestamp() - $this->timestamp->getTimestamp());
 		if ($differenceInSeconds > self::TIMESTAMP_VALID_TOLERANCE_SECONDS) {
-			throw new InvalidArgumentException('Request timestamp was too old. Possible replay attack.');
+			self::handleException(new InvalidArgumentException('Request timestamp was too old. Possible replay attack.'));
 		}
 	}
 }
